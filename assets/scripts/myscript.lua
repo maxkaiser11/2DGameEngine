65ba84126e3926cec677aececc08d3f4a6110209
Max Kaiser <max@kaiser.family>
2025-09-13 01:11:42 +0200

    Got Sol2 and Lua 5.4 to work

---
+++ assets/scripts/myscript.lua
@@ -0,0 +1,7 @@
+-- This is a lua global variable
+some_variable = 7 * 6
+user_name = "Gustavo"
+
+if some_variable == 42 then
+    print("The value of the variable is " .. 42)
+end
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,28 @@
 #include "./Game/Game.h"
+#include <sol/sol.hpp>
+#include <iostream>
+
+void TestLua()
+{
+    sol::state lua;
+
+    lua.open_libraries(
+        sol::lib::base
+        );
+    lua.script_file("./assets/scripts/myscript.lua");
+
+    int someVarInsideCpp = lua["some_variable"];
+    std::cout << "Some lua var " << someVarInsideCpp << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    Game game;
+    // Game game;
+    //
+    // game.Initialize();
+    // game.Run();
+    // game.Destroy();
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+    TestLua();
 
     return 0;
 }
